b18355bc4f2ec9fd3aa861b865a0c312ab369292
Austin-ALIT <austin@al.co.za>
2023-07-07 12:51:16 +0200

    Added Posts Carousel to Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,71 @@
 // Landing Page
 
 import Link from "next/link";
+import styles from '../styles/components/homepage.module.css';
 
 // Component Imports
 import Housing from "@/Components/housing/housing";
+import PostCarousel from "@/Components/Posts/postcarousel";
 
 export default function Home(){
   
   return(
-      <div>
-          <Housing pagename="Home">
-            <h1> Welcome Home </h1>
-            <br/> <br/>
-            <p> 
+      <>
+      <Housing pagename="Home">
+
+        {/* Header */}
+        <div className={styles.headerstuff}> 
+          <h1 className={styles.headertext}> Welcome Home </h1>
+        </div>
+        <br/> <br/>
+        <div className={styles.homepage}>
+            <p className={styles.homepageDialogue}> 
               Why don't you kick back, get yourself a cup of tea (or whatever you fancy) and put on some smooth 
-              tunes as you experience some adventures with me, whad'ya say? 
+              tunes as you experience some adventures with me, 
             </p>
-            <br/>
-            <p> Latest posts carousel </p>
-            <p> See all posts (link) </p>
-            <p> Or do these give you an idea? If so, suggest a post! (link)</p>
-            <br/>
-            <p> But Why? (insert buy why gif here maybe?) </p>
-            <p> 
-              I believe that everyone has droves of stories, but it can sometimes be hard to tell them with the perfect delivery. 
-              That's why I would love to hear your stories as well, so feel free to [suggest a post] and I will 
-              help you as I can on gathering your most powerful delivery for those you want to hear it. 
+            <p>
+              whad'ya say? 
             </p> 
-            <br/>
-            <p> 
-              Or, if you you can relate to whatever it is you might be reading here, feel free to [leave a comment
-              on one of the other posts] as you go by, or [suggest a post of your own].
-            </p>
-            <br/>
-            <p> Want to know even more [about me] or [about my blog]? Click the relevant links to find out! </p>
-            <br/><br/>
-          </Housing>
-      </div>
+            <br/> <br/>
+        </div>
+
+        {/* Posts */}
+        <div className={styles.headerstuff}> 
+          <p className={styles.headertext}> Recent Posts </p>
+        </div>
+        <br/>
+        <PostCarousel/>
+        <div className={styles.homepage}>
+          <p> <Link href="/Posts" className={styles.linkstuff}>See all posts</Link> </p>
+          <p> Or do these give you an idea? If so, <Link href="/newpost" className={styles.linkstuff}>suggest a post</Link>!</p>
+          <br/> <br/>
+        </div>
+
+        {/* About Preview */}
+        <div className={styles.headerstuff}> 
+          <p className={styles.headertext}> But Why? </p>
+        </div>
+        <br/> <br/>
+        <div className={styles.homepage}>
+          <p className={styles.homepageDialogue}> 
+            I believe that everyone has droves of stories, but it can sometimes be hard to tell them with the perfect delivery. 
+            That's why I would love to hear your stories as well, so feel free 
+            to <Link href="/newpost" className={styles.linkstuff}>suggest a post</Link> and I will 
+            help you as I can on gathering your most powerful delivery for those you want to hear it. 
+          </p> 
+          <br/> <br/>
+          <p className={styles.homepageDialogue}> 
+            Or, if you you can relate to whatever it is you might be reading here, feel free to leave a comment
+            on one of the <Link href="/Posts" className={styles.linkstuff}>other posts</Link> as you go by, 
+            or <Link href="/newpost" className={styles.linkstuff}>suggest a post of your own</Link>.
+          </p>
+          <br/>
+          <p> 
+            Want to know even more <Link href="/aboutme" className={styles.linkstuff}> about me</Link> or <Link href="/aboutblog" className={styles.linkstuff}>about my blog</Link>? </p> 
+          <p> Click the relevant links to find out! </p>
+        </div>
+        <br/><br/>
+      </Housing>
+      </>
   );
 };
