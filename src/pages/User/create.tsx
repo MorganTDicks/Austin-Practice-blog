36265dc7eb773f282c8b9bfa426629674c578a0b
Austin-ALIT <austin@al.co.za>
2023-07-21 12:10:20 +0200

    Finished create account functionality

--- src/pages/User/create.tsx
+++ src/pages/User/create.tsx
@@ -6,29 +6,54 @@
 
 import MainLayout from "@/Layouts/mainlayout/mainlayout";
 import Link from "next/link";
-import { FormEvent, useState } from "react";
-import { User } from "@/Declarations/UserTypes";
+import { FormEvent, useContext, useEffect, useState } from "react";
 import DataImporter from "@/Utilities/dataimporter";
 import GenericInput from "@/Components/generic/genericinput";
+import userContext from "@/Context/datawrappers/userwrapper";
+import { User } from "@/Declarations/UserTypes";
+import { useRouter } from "next/router";
 
 // Redirect to dashboard once account created
 
 
 export default function CreateAccount(){
     
-    const [newUser, setNewUser] = useState<User>(DataImporter.initialUser);
+    const [newUser, setNewUser] = useState({...(DataImporter.initialUser), password: ''});
+    let [isValid, setIsValid] = useState<boolean>(false);
+    let contex = useContext(userContext);
+    let rout = useRouter();
+    
+    // Input Checking
+    useEffect(()=>{
+        let timia = setTimeout(()=>{
+            // Check input validity
+            if ((newUser.email.indexOf('@') !== -1) && (newUser.password.length >= 8) && (newUser.name.length > 0) && (newUser.surname.length > 0)){
+                setIsValid(true);
+            }
+        }, 500)
+
+        return() => {
+            clearTimeout(timia);
+        }
+    }, [newUser])
 
     function createUserAccountHandler(event: FormEvent<HTMLFormElement>){
-            /* ... */
             event.preventDefault();  
-            // TODO - Input Checking
             
-            // Adding user requires states, pushing data up or context. Either way, i can't do this yet. 
-            console.log(newUser);
+            // Calculate ID
+            newUser.id = calcID(newUser.name, newUser.surname, contex.value);
+
+            // Saving new User.
+            contex.changer(newUser);
+            console.log(contex.value[contex.value.length-1]);
+
+            // TODO: Save Password
 
             // Clearing the forms once the user has been added. 
-            setNewUser(DataImporter.initialUser);
+            setNewUser({...(DataImporter.initialUser), password: ''});
 
+            // redirect to the login screen
+            rout.push('/User/login');
 
     }
 
@@ -52,10 +77,6 @@ export default function CreateAccount(){
         } 
     }
 
-
-
-    // function userInfoHandler(id, value) () {} // ID will be calculated when submit button is clicked. 
-
     return(
         <>
             <MainLayout pagename ="Create Account">
@@ -67,34 +88,54 @@ export default function CreateAccount(){
                         <tbody> 
                             <tr>
                                 <GenericInput label="E-Mail" type="email" onChange={(event: any) => inputUpdateHandler('email', event.target.value)} value={newUser.email}/> 
-                                {/*<td> <label> E-Mail </label> </td>
-                                <td> <input type="text" onChange={(event) => inputUpdateHandler('email', event.target.value)} value={newUser.email}/> </td> */}
                             </tr>
                             <tr>
                                 <GenericInput label="First Name" type="text" onChange={(event: any) => inputUpdateHandler('name', event.target.value)} value={newUser.name}/>
-                                {/* <td> <label> First Name </label> </td>
-                                <td> <input type="text" onChange={(event) => inputUpdateHandler('name', event.target.value)} value={newUser.name}/> </td> */}
                             </tr>
                             <tr>
                                 <GenericInput label="Last Name" type="text" onChange={(event: any) => inputUpdateHandler('surname', event.target.value)} value={newUser.surname}/>
-                                {/* <td> <label> Last Name </label> </td>
-                                <td> <input type="text" onChange={(event) => inputUpdateHandler('surname', event.target.value)} value={newUser.surname}/> </td> */}
                             </tr>
                             <tr>
                                 <GenericInput label="Username" type="text" onChange={(event: any) => inputUpdateHandler('username', event.target.value)} value={newUser.username}/>
-                                {/* <td> <label> Username </label> </td>
-                                <td> <input type="text" onChange={(event) => inputUpdateHandler('username', event.target.value)} value={newUser.username}/> </td> */}
                             </tr>
                             <tr>
-                                {/* <GenericInput label="Password" type="text" onChange={(event: any) => inputUpdateHandler('password', event.target.value)} value={newUser.password}/> */}
-                                {/* <td> <label> Password </label> </td>
-                                <td> <input type="password" onChange={(event) => inputUpdateHandler('password', event.target.value)}/> </td> */}
+                                <GenericInput label="Password" type="password" onChange={(event: any) => inputUpdateHandler('password', event.target.value)} value={newUser.password}/>
                             </tr>
                         </tbody>
                     </table>
-                    <button type="submit"> Register </button>
+                    <button type="submit" disabled={!(isValid)}> Register </button>
                 </form>
             </MainLayout>
         </>
     )
+}
+
+function calcID(firstname: string, surname: string, arrUsers: User[]){
+    // Copy first letter of firstname
+    let fnn = firstname.slice(0,1);
+
+    // Copy first letter of lastname
+    let lnn = firstname.slice(0,1);
+
+    // Generate random number for remaining characters
+    let rand = Math.floor(Math.random() * (999999 - 99999) + 99999); // Expected random between 99999 & 999999
+
+    // Check if number already taken, if so, incriment by 1. 
+    let pass = true;
+    do{
+        for (let u of arrUsers){
+            if (u.id.slice(0,2) == `${fnn}${lnn}`){
+                if (+u.id.slice(3,9) == rand){
+                    rand += 1; 
+                    pass=false;
+                }
+            }
+        }
+    } while(!pass)
+    
+    let finalID = `${fnn}${lnn}${rand}`;
+
+    // Sample: "AA000000"
+    // Received: "AA678430"
+    return(finalID);
 }
